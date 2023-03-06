9e925a2e4e4a9dfdbca1ebcfb4043defc1da7695
Ginver <74971813+Ginver@users.noreply.github.com>
2023-03-06 17:52:14 +0100

    main.js trying to make a function to let the popup appear

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-// function popup() {
-//     alert("You clicked me!")
-// }
+const popup = document.querySelector("popup--social")
+const btn = document.querySelector("button");
+function popupSocial() {
+    popup.style.visibility = "visible";
+}
+    btn.addEventListener("click", popupSocial);
+    function popupSocial() {
+        popup.style.visibility = "visible";
+    }
+

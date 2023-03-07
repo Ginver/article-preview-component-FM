fb80cbc7cffcebec2f44b867ec4f50197cd360d3
Ginver <74971813+Ginver@users.noreply.github.com>
2023-03-07 20:14:28 +0100

    main.js popup fixed

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-const popup = document.querySelector("popup--social")
-const btn = document.querySelector("button");
+const popup = document.querySelector(".popup--social")
+// console.log(popup)
+const btn1 = document.querySelector(".btn__arrow");
+const btn2 = document.querySelector(".btn__arrow--responsive");
+
+btn1.addEventListener("click", popupSocial);
+btn2.addEventListener("click", popupSocial);
+
 function popupSocial() {
-    popup.style.visibility = "visible";
-}
-    btn.addEventListener("click", popupSocial);
-    function popupSocial() {
+    // console.log("test")
+    if (popup.style.visibility === "visible") {
+        popup.style.visibility = "hidden";
+    } else {
         popup.style.visibility = "visible";
     }
+}
 
